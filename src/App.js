0eb8ee37b55023337dd8c8c97586306f12ab2844
khad.work20@gmail.com <khd.work20@gmail.com>
2020-08-07 03:26:32 +0700

    TUGAS14

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import TabelHargaBuah from './tugas11/TabelHargaBuah';
 import Timer from './tugas12/waktu';
 import TabelHarga from './tugas13/TambahElemen';
+import TabelHargaFruit from './tugas14/tugas14';
 
 function App() {
   return (
     <div className="App">
       {/* <TabelHargaBuah/> */}
-      <TabelHarga/>
+      {/* <TabelHarga/> */}
+      <TabelHargaFruit/>
       <Timer start={114}/>
       
     </div>
